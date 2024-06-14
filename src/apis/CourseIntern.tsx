8aea2dd59b8b3fa03d691ace331591a47812501f
AzureXen <phamdanminh@gmail.com>
2024-06-14 12:22:17 +0700

    Added View Course for Students

---
+++ src/apis/CourseIntern.tsx
@@ -0,0 +1,21 @@
+
+import axios from "axios";
+import CourseIntern from "../model/CourseIntern"
+const fetchCoursesForIntern = async (internId : string) => {
+    try {
+        const response =
+            await axios.get(`http://localhost:8080/internbridge/intern/allCourse/${internId}`);
+            return response.data.map((courseIntern: any) => new CourseIntern(
+                courseIntern.course.id,
+                courseIntern.course.company.id,
+                courseIntern.course.courseDescription,
+                courseIntern.course.mentor.fullName,
+                courseIntern.id.internId
+            ));
+    } catch (error) {
+        console.error("Error fetching courses for intern", error);
+        throw error;
+    }
+};
+
+export default fetchCoursesForIntern;
